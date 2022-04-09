ba7bc6ce6e2dcfab93f848ebcdb63a944dbc6524
AntonioMaldonado <antonio.manilla.maldonado@gmail.com>
2022-04-08 20:06:49 -0500

    Trying first icons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './clock_logo_2.png';
 import './App.css';
 
 function App() {
@@ -6,20 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Mi first Pipeline
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
-}
+
+}//End of App()
 
 export default App;
