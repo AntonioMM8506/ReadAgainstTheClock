a3f32410bdb1f995112c4f54489983609edc141b
AntonioMaldonado <antonio.manilla.maldonado@gmail.com>
2022-04-15 15:12:25 -0500

    Timer component redefinition

--- src/Components/timer.js
+++ src/Components/timer.js
@@ -1,47 +1,45 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './timer.css';
 
 const Timer = () => {
+    //Hooks approach
+    const [time, setTime] = React.useState(0);
+    const [timerOn, setTimeOn] = React.useState(false);
+    
+    //Uses an interval that can be modified with the setinterval method
+    //inside this method one can define the conditions mo modify the given
+    //interval. With the method clearInterval, the given interval is stoped
+    //However, because of the hooks it is not reset as in a normal use.
+    React.useEffect(() => {
+        let interval = null;
 
-    const [seconds, setSeconds] = useState(0);
-    const [minutes, setMinutes] = useState(0);
+        if(timerOn){ //Timer starts
+        interval = setInterval(() => {
+            setTime(prevTime => prevTime + 1);
+        }, 1000);
+        }else{ //Timer stops
+        clearInterval(interval);
+        }
 
-    var timer;
-    var isPaused = false;
-    useEffect(()=> {
+        return () => clearInterval(interval); // to avoid memory leaks
 
-            timer = setInterval(() =>{
-                setSeconds(seconds+1);
-                if(seconds==59){
-                    setMinutes(minutes+1);
-                    setSeconds(0);
-                }
-            },1000);
-            return () => clearInterval(timer);
-    });
+    }, [timerOn]); //runs every time the variable timeOn changes
 
-    const restart = () =>{
-        setSeconds(0);
-        setMinutes(0);
-    }
-
-    const stop = () => {
-        clearInterval(timer);
-    }
-
-    const resume = () => {
-        
-    }
-
-
+    //Render
     return (
         <div className='container'>
             <div className='timer_container'>
-                <h1>Timer</h1>
-                <h1>{minutes<10 ? "0"+minutes:minutes}:{seconds<10 ? "0"+seconds:seconds}</h1>
-                <button className='restart' onClick={restart}>Restart</button> 
-                <button className='stop' onClick={stop}>Stop</button>
-                <button className='resume' onClick={resume}>Resume</button>
+            <h1>
+                <span>{("0" + Math.floor((time/6000)%60)).slice(-2)}:</span> {/*Hours*/}
+                <span>{("0" + Math.floor((time/1000)%60)).slice(-2)}:</span> {/*minutes*/}
+                <span>{("0" + ((time)%100)).slice(-2)}</span> {/*Seconds*/}
+            </h1>
+                <div>
+                    <button className='resume' onClick={() => setTimeOn(true)}>Start</button>
+                    <button className='stop' onClick={() => setTimeOn(false)}>Stop</button>
+                    <button className='resume' onClick={() => setTimeOn(true)}>Resume</button>
+                    <button className='reset' onClick={() => setTime(0)}>Reset</button> 
+                </div>
             </div>
         </div>
 
