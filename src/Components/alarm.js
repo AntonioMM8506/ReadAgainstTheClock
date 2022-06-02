c3ab4a36b1c1ea0c96a66586a6ce5aa1f6c91203
AntonioMaldonado <antonio.manilla.maldonado@gmail.com>
2022-06-01 21:13:20 -0500

    First approach for Setting an alarm
    popup and alarm components
    The alarm can only be set for minutes
    There's one sound for the alarm only
    the css files need to be merged or reduced
    the popup are only displayed for 1 minute

---
+++ src/Components/alarm.js
@@ -0,0 +1,34 @@
+import React from "react";
+import './alarm.css';
+import alarm from './img/alarm.png';
+import ringer from './sound/alarm_sound1.mp3'
+
+function Alarm( props ) {
+
+    const audio = new Audio(ringer);
+    audio.loop = true;
+
+    function refreshPage() {
+        setTimeout(function(){
+            window.location.reload(true);
+        }, 50000);
+
+    }
+
+    return (props.trigger) ? (
+
+        <div className="popupAlarm" >
+            <div className="popupAlarm-inner" onLoad={ () => { 
+            audio.loop = true;
+            audio.play(); 
+            refreshPage();
+            }}>
+                <img src={alarm} id='alarmLogo' alt="logo"  />
+                <button className="close-btn" onClick={ () =>  window.location.reload() }>Close</button>
+                {props.children}
+            </div>
+        </div>
+    ) : "";
+}//End of Popup
+
+export default Alarm;
--- src/Components/timer.js
+++ src/Components/timer.js
@@ -1,12 +1,9 @@
 import logo from './img/clock_logo_3.png';
 import * as React from 'react';
 import './timer.css';
-//mui libraries
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+import Popup from './popup';
+import Alarm from './alarm';
+
 
 const Timer = () => {
     //Hooks approach ................................................................
@@ -42,97 +39,113 @@ const Timer = () => {
     };
 
     //Set Limit Logic ..................................................................
+    const [buttonTriger, setButtonTrigger] = React.useState(false);
+    
     const [limit, setLimit] = React.useState(0);
-
+    //console.log("Starter limit: ", limit);
+    
     const handleChange = (event) => {
         setLimit(event.target.value);
-        console.log(limit);
+        //console.log("New given limit", limit);
     };
-
-    //Array for the list of options to  be displayed in the selection box
-    const minutesItems = [];
-    for(let i=1; i<=60; i++){
-        minutesItems.push(<MenuItem value={ i }>{i<10 ? "0"+i : i}</MenuItem>)
+    
+    const minutesSelect = [];
+    for(let minSel=0; minSel<61; minSel++){
+        minutesSelect.push(<option value={minSel}>{minSel}</option>);
     }
 
-    //In case, an option has been selected from the Selection Box, an alarm will be triggered
+    //In case, an option has been selected from the Selection Box, 
+    //an alarm will be triggered
     if(limit !== 0){
-        if(limit === Math.floor((time/60000)%60)){
-            alert("it's time");
+        //console.log('Current limit is ', parseInt(limit,10));
+        //console.log('Current time is:',  Math.floor((time/60000)%60));
+
+        if(parseInt(limit,10) === Math.floor((time/60000)%60)){
+            return(
+                <div onLoad={() => {
+                }}>
+                <Alarm trigger='true'>
+                    <h2>Time's Up!</h2>
+                    <h3>Close to Reload</h3>
+                </Alarm>
+                </div>
+            )
+            
         }
     }
+    
 
     //Render ------------------------------------------------------------------------------------
     return (
-        <div className='container'>
-            {/*Logo will spin if the clock is running*/}
-            <img src={logo} id='App-logo' className='App-logo-run' style={{animationPlayState: isActive? 'running':'paused' }} alt="logo" />
+        <div className='Main'>
+            <div className='container'>
+                {/*Logo will spin if the clock is running*/}
+                <img src={logo} id='App-logo' className='App-logo-run' style={{animationPlayState: isActive? 'running':'paused' }} alt="logo" />
 
-            {/*Select Box, it will display the 60 minutes as options to be set*/}
-            <Box sx={{ 
-                    minWidth: 120,
-                    '&:hover': {
-                        backgroundColor: 'primary.main',
-                        opacity: [0.9, 0.8, 0.7],
-                    },
-                }}>
-                <FormControl fullWidth>
-                    <InputLabel id="demo-simple-select-label">Set Minute Alert</InputLabel>
-                    <Select
-                        labelId="demo-simple-select-label"
-                        id="demo-simple-select"
-                        value={ limit }
-                        label="setLimit"
-                        onChange={handleChange}
-                    >
-                    { minutesItems }
-                    </Select>
-                </FormControl>
-            </Box>
-            <br></br>
+                {/*Select Box, it will display the 60 minutes as options to be set*/}
+                <br></br>
+                <button id='actionButton' className='setTimer' onClick={() => {
+                    setButtonTrigger(true);
+                }}>Set Timer</button>
+                <br></br>
+
+
+                
+                {/*Timer and its action buttons*/}
+                <div className='timer_container'>
+                    <h1>
+                        <span>{("0" + Math.floor((time/60000)%60)).slice(-2)} :</span> {/*minutes*/}
+                        <span>{("0" + Math.floor((time/1000)%60)).slice(-2)}</span> {/*seconds*/}
+                        {/* <span>{("0" + ((time/10)%100)).slice(-2)}</span> */}{/*miliseconds*/}
+                    </h1>
+                    <div>
+                        {/*Run*/}
+                        {   
+                            time === 0 ? 
+                            <button id='actionButton' className='start' onClick={() => {
+                                toggleClass();
+                                setTimeOn(true);
+                                document.getElementById('App-logo').classList.add("App-logo-run");
+                            }}>Start</button> 
+                            :
+                            <button id='actionButton' className='resume' onClick={() => {
+                                toggleClass();
+                                setTimeOn(true);
+                            }}>Resume</button>
+                        }
+
+                        {/*Pause/Stop*/}
+                        <button id='actionButton' className='stop' onClick={() => {
+                            setTimeOn(false);
+                            negativeToggleClass();
+                        } }>Pause</button>
+
+                        {/*Reset*/}
+                        <button id='actionButton' className='reset' onClick={() =>{
+                            setTime(0);
+                            setTimeOn(false);
+                            negativeToggleClass();
+                            document.getElementById('App-logo').classList.remove("App-logo-run");
+                        
+                        }}>Reset</button>
+                        
+                    </div>
+                </div>{/*End of timer*/}
+        </div>{/*End of timer container*/}
             
-            {/*Timer and its action buttons*/}
-            <div className='timer_container'>
-                <h1>
-                    <span>{("0" + Math.floor((time/60000)%60)).slice(-2)} :</span> {/*minutes*/}
-                    <span>{("0" + Math.floor((time/1000)%60)).slice(-2)}</span> {/*seconds*/}
-                    {/* <span>{("0" + ((time/10)%100)).slice(-2)}</span> */}{/*miliseconds*/}
-                </h1>
-                <div>
-                    {/*Run*/}
-                    {   
-                        time === 0 ? 
-                        <button id='actionButton' className='start' onClick={() => {
-                            toggleClass();
-                            setTimeOn(true);
-                            document.getElementById('App-logo').classList.add("App-logo-run");
-                        }}>Start</button> 
-                        :
-                        <button id='actionButton' className='resume' onClick={() => {
-                            toggleClass();
-                            setTimeOn(true);
-                        }}>Resume</button>
-                    }
+            {/*Popup message displayed in order to input the expected minutes*/}
+            <Popup trigger = {buttonTriger} setTrigger = {setButtonTrigger}>
+                <h2>Set the Timer</h2>
+                <h3>An alarm will be played when the time is up</h3>
+                <label>
+                    Minutes:&nbsp;  
+                    <select name='Minutes' value={limit} onChange={handleChange}>
+                        {minutesSelect}
+                    </select>
+                </label>
+            </Popup>
 
-                    {/*Pause/Stop*/}
-                    <button id='actionButton' className='stop' onClick={() => {
-                        setTimeOn(false);
-                        negativeToggleClass();
-                    } }>Pause</button>
-
-                    {/*Reset*/}
-                    <button id='actionButton' className='reset' onClick={() =>{
-                        setTime(0);
-                        setTimeOn(false);
-                        negativeToggleClass();
-                        document.getElementById('App-logo').classList.remove("App-logo-run");
-                    
-                    }}>Reset</button>
-                    
-                </div>
-            </div>{/*End of timer*/}
-        </div>
-
+        </div>           
     )//End of return
 
 }//End of Timer
